5ad96581b7d10eaa166ecbe51a0dd7b60484003c
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2024-04-01 08:15:58 +0530

    Add files via upload

---
+++ ArmstrongNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class ArmstrongNumber{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("\nEnter the number : ");
+		int num=sc.nextInt();
+		int value=num;
+		int sum=0;
+		String temp=Integer.toString(num);
+		int len=temp.length();
+		while(num!=0){
+			int rem=num%10;
+			sum+=(int)Math.pow(rem,len);
+			num/=10;
+		}
+		if(value==sum){
+			System.out.println("\n"+value+" -> Is Armstrong Number.");
+		}else{
+			System.out.println("\n"+value+" -> Is Not Armstrong Number..!");
+		}
+	}
+}
