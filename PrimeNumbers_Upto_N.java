c4918c355d02d0732a4763f7d3797c3d30cd7e16
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2024-04-02 22:46:03 +0530

    Add files via upload

---
+++ PrimeNumbers_Upto_N.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class PrimeNumbers_Upto_N{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.print("\nEnter the size upto where you need to find primes : ");
+		int size=sc.nextInt();
+		
+		if(size<0){
+			System.out.println("\n"+size+" -> Size Can't Be Negative !!");
+			sc.close();
+			return;
+		}
+		sc.close();
+		
+		System.out.println("\nPrime Numbers Upto -> "+size);
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		
+		for(int i=2;i<=size;++i){
+			if(isPrime(i)){
+				System.out.print(i+",");
+			}
+		}
+	}
+	public static boolean isPrime(int num){
+		if(num <= 1)
+			return false;
+		
+		for(int i=2;i<=Math.sqrt(num);++i){
+			
+			if(num % i == 0){
+				return false;
+			}
+		}
+		return true;
+	}
+}
