2368f8012373c5e357f3a1aca682b3ea9dadcfc3
Y Venkata Ravi <156241322+VenkataRavi2003@users.noreply.github.com>
2024-04-02 22:47:17 +0530

    Update PrimeNumbers_Upto_N.java

--- PrimeNumbers_Upto_N.java
+++ PrimeNumbers_Upto_N.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class PrimeNumbers_Upto_N{
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
-		
 		System.out.print("\nEnter the size upto where you need to find primes : ");
 		int size=sc.nextInt();
 		
@@ -15,7 +14,6 @@ public class PrimeNumbers_Upto_N{
 		
 		System.out.println("\nPrime Numbers Upto -> "+size);
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-		
 		for(int i=2;i<=size;++i){
 			if(isPrime(i)){
 				System.out.print(i+",");
@@ -25,9 +23,7 @@ public class PrimeNumbers_Upto_N{
 	public static boolean isPrime(int num){
 		if(num <= 1)
 			return false;
-		
 		for(int i=2;i<=Math.sqrt(num);++i){
-			
 			if(num % i == 0){
 				return false;
 			}
